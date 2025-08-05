b0ebed8aca31b00ccec9a7c4fdf85c7c7bc2e910
Ohshocl <Ohshocl@gmail.com>
2025-08-05 15:09:02 -0600

    Update create-checkout.js

--- api/shopify/create-checkout.js
+++ api/shopify/create-checkout.js
@@ -294,6 +294,6 @@ export default async function handler(req, res) {
 // =================================================================
 
 export const config = {
-    runtime: 'nodejs18.x',
+    runtime: 'nodejs',
     maxDuration: 30
 };
